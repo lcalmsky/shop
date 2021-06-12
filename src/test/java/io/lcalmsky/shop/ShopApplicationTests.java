589dcb586124b2bab769112112c298712f7c8d12
Jaime <lcalmsky@gmail.com>
2021-06-12 23:22:22 +0900

    Initial commit

---
+++ src/test/java/io/lcalmsky/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lcalmsky.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
