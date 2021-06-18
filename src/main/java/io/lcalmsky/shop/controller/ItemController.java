86f68cf120cd479be5a86876dc7f4abfeab19e36
Jaime <lcalmsky@gmail.com>
2021-06-19 00:37:59 +0900

    Develop item register function

---
+++ src/main/java/io/lcalmsky/shop/controller/ItemController.java
@@ -0,0 +1,33 @@
+package io.lcalmsky.shop.controller;
+
+import io.lcalmsky.shop.domain.item.Book;
+import io.lcalmsky.shop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setIsbn(form.getIsbn());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setPrice(form.getPrice());
+        book.setAuthor(form.getAuthor());
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
