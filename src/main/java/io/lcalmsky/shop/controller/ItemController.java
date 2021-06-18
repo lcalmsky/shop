8259497048f988cbd9a0d54872a3c81d26825cf0
Jaime <lcalmsky@gmail.com>
2021-06-19 01:30:55 +0900

    Develop item update function

--- src/main/java/io/lcalmsky/shop/controller/ItemController.java
+++ src/main/java/io/lcalmsky/shop/controller/ItemController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -35,5 +37,31 @@ public class ItemController {
     public String list(Model model) {
         model.addAttribute("items", itemService.findItems());
         return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book book = (Book) itemService.findOne(itemId);
+        BookForm bookForm = new BookForm();
+        bookForm.setId(book.getId());
+        bookForm.setAuthor(book.getAuthor());
+        bookForm.setStockQuantity(book.getStockQuantity());
+        bookForm.setIsbn(book.getIsbn());
+        bookForm.setName(book.getName());
+        model.addAttribute("form", bookForm);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form, @PathVariable Long itemId) {
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setIsbn(form.getIsbn());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setPrice(form.getPrice());
+        book.setAuthor(form.getAuthor());
+        itemService.saveItem(book);
+        return "redirect:/items";
     }
 }
